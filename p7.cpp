ae67765d8117388faf848f90d2d76c418271be74
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-09-13 19:47:19 -0700

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+enum AND{FALSE,TRUE};
+int main()
+{
+	cout<<"janvi c. vadhiya"<<endl;
+	cout<<"220130318031"<<endl;
+	cout<<"A"<<"\t"<<"b"<<"\t"<<"Y"<<endl;
+	cout<<FALSE<<"\t"<<FALSE<<"\t"<<FALSE*FALSE<<endl;
+	cout<<FALSE<<"\t"<<TRUE<<"\t"<<FALSE*TRUE<<endl;
+	cout<<TRUE<<"\t"<<FALSE<<"\t"<<TRUE*FALSE<<endl;
+	cout<<TRUE<<"\t"<<TRUE<<"\t"<<TRUE*TRUE<<endl;
+	return 0;
+}
