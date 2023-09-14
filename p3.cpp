57ea7470e8b20dcc482e27029c1cb5178303222e
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-09-13 19:46:02 -0700

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class temp
+{
+	public:
+		void display()
+{
+	cout<<"hello welcome to DSU"<<endl;
+}
+};
+int main()
+{
+	temp t1;
+	t1.display();
+	return 0;
+}
