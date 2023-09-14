68847fb329eafdde9761148e7b1af3d4eea620cc
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-09-13 19:46:42 -0700

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	
+
+	float pi=3.14285;
+	
+	cout<<"janvi c. vadhiya"<<endl;
+	cout<<"220130318031"<<endl;
+	cout<<"value of pi is::"<<pi<<endl;
+	cout<<"aafter applying precision::"<<setprecision(3)<<pi;
+}
